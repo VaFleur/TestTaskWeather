24b54f88d1ec3380a8ab32f528ec885003936cfe
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-07 17:27:43 +0700

    Added News serializer

---
+++ TestTaskWeather/news/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import News
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'main_image', 'preview_image', 'content', 'publication_date', 'author']

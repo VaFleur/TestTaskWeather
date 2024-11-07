750162ec920c9fb4f62bc55db7aa8fda628f3a79
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-07 17:56:14 +0700

    Created NewsAdmin in news.admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import News
 
-# Register your models here.
+
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+
+admin.site.register(News, NewsAdmin)

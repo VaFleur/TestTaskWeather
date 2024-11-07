c5cf1066a8bbc1fd76ad18e7c4ebcc736237f31a
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-07 17:53:44 +0700

    Configured summernote

--- TestTaskWeather/urls.py
+++ TestTaskWeather/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('news.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]

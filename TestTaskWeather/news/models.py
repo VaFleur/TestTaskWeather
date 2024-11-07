407c914c7e4a64a2950acdef6b49e33db4c03215
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-07 17:22:23 +0700

    Added News to models

--- TestTaskWeather/news/models.py
+++ TestTaskWeather/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    main_image = models.ImageField(upload_to='news_images/')
+    preview_image = models.ImageField(upload_to='news_images/previews/', blank=True, null=True)
+    content = models.TextField()
+    publication_date = models.DateField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title

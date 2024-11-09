894f9042f4f8633941ec500bc603f86a88f25b70
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-09 17:59:39 +0700

    Added export to admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import News, Place, WeatherReport
+from .utils import export_weather_to_xlsx
 
 
 class NewsAdmin(SummernoteModelAdmin):
@@ -20,3 +21,14 @@ class PlaceAdmin(admin.ModelAdmin):
 class WeatherReportAdmin(admin.ModelAdmin):
     list_display = ('place', 'temperature', 'humidity', 'pressure', 'wind_speed', 'report_date')
     list_filter = ('place', 'report_date')
+    actions = ['export_selected']
+
+    def export_selected(self, request, queryset):
+        place = request.GET.get('place')
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+        response = export_weather_to_xlsx(place, start_date, end_date)
+        return response
+
+    export_selected.short_description = "Экспортировать в xlsx"
+
--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NewsViewSet
+from .views import NewsViewSet, export_weather_view
 
 router = DefaultRouter()
 router.register(r'news', NewsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('export-weather/', export_weather_view, name='export_weather'),
 ]

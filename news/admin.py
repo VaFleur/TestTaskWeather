2ce4deb047c2ac9e53b016aadf259b7dcd90f6d9
Anton Zhilinskey <zhilinskiianton@gmail.com>
2024-11-09 17:47:10 +0700

    Added weather report to admin

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import News, Place
+from .models import News, Place, WeatherReport
 
 
 class NewsAdmin(SummernoteModelAdmin):
@@ -14,3 +14,9 @@ admin.site.register(News, NewsAdmin)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('name', 'coordinates', 'rating')
     search_fields = ('name',)
+
+
+@admin.register(WeatherReport)
+class WeatherReportAdmin(admin.ModelAdmin):
+    list_display = ('place', 'temperature', 'humidity', 'pressure', 'wind_speed', 'report_date')
+    list_filter = ('place', 'report_date')
